5861b3ee049c6bfc600afd4ce861eac23499e6e8
Atti <raphael_mayer@hotmail.de>
2018-09-19 14:39:00 +0200

    Synth redone
    Synth redone as class component;
    added a log feature to test mobile behaviour;

--- src/components/Synth.jsx
+++ src/components/Synth.jsx
@@ -1,40 +1,113 @@
-import React from "react";
+import React, { Component } from "react";
 import "./css/Synth.css";
 
 import readOutLoud from "../helpers/readOutLoud";
 
-const Synth = ({ toRead }) => {
+class Synth extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { logIsOpen: false, toRead: "Test Phrase" };
+		this.handleLog = this.handleLog.bind(this);
+		this.handlePlayButton = this.handlePlayButton.bind(this);
+		this.handleSkipButton = this.handleSkipButton.bind(this);
+		this.handleStopButton = this.handleStopButton.bind(this);
+		this.handleTestButton = this.handleTestButton.bind(this);
+	}
+	componentDidMount() {
+		console.log(this.props)
+		const { toRead } = this.props;
+		toRead && this.setState({ toRead });
+	}
 
+	// button handlers
+	handlePlayButton(e) {
+		// playbutton(window.speechSynthesis, this.state.toRead);
+		const playbtn = document.getElementById("playbtn");
+		const synth = window.speechSynthesis;
+		
+		// differentiate between
+		// START || PAUSE || RESUME
+		if (!synth.speaking) {
+			print("START EVENT");
+			readOutLoud(this.state.toRead);
+			this.setState({ synthState: "ON" });
+		} else if (synth.paused) {
+			print("RESUME EVENT");
+			synth.resume();
+			this.setState({ synthState: "ON" });
+		} else if (synth.speaking && !synth.paused) {
+			print("PAUSE EVENT");
+			synth.pause();
+			this.setState({ synthState: "PAUSE" });
+		} else { 
+			print("ERROR EVENT");
+			alert("ERROR");
+		};
+	}
+	handleSkipButton(e) {
+		print("SKIP EVENT");
+		alert("SKIP: TO DO");
+	}
+	handleStopButton(e) {
+		const synth = window.speechSynthesis;
+		// synth.paused && synth.resume(); evtl ? 
+		print("STOP EVENT");
+		synth.cancel();
+		this.setState({ synthState: "OFF" });
+	}
+	handleTestButton(e) {
+		print("TEST EVENT");
+		readOutLoud(this.state.toRead);
+		this.setState({ synthState: "ON" });
+	}
+
+	handleLog(e) {
+		this.state.logIsOpen ? 
+		this.setState({ logIsOpen: false })
+		:
+		this.setState({ logIsOpen: true }); 
+	}
+
+	render() {
+		const { toRead, logIsOpen, synthState } = this.state;
+		return (
+			<div className="Synth">
+
+				<i onClick={ this.handleLog } className="fas fa-bars"></i>
+				<div id="log" className={ logIsOpen ? "" : "hidden" }></div>
+				
+				<SynthBtn icon={ synthState === "ON" ? "fas fa-pause" : "fas fa-play" } onClick={ this.handlePlayButton } />
+
+				<SynthBtn icon="fas fa-angle-double-right" onClick={ this.handleSkipButton } />
+
+				<SynthBtn icon="fas fa-stop" onClick={ this.handleStopButton } />
+
+				<SynthBtn icon="fas fa-volume-down" onClick={ this.handleTestButton } />
+
+			</div>
+		);
+	}
+}
+
+export default Synth;
+
+const SynthBtn = ({ icon, onClick }) => {
 	return (
-		<div className="Synth">
-			<div  className="synth-button">
+		<div  className="synth-button">
 			<div className="inner">
-				<i className="fas fa-play" id="playbtn" onClick={ () => playbutton(window.speechSynthesis, toRead) } ></i>
-			</div>
-			</div>
-			<div  className="synth-button">
-			<div className="inner">
-				<i className="fas fa-angle-double-right" onClick={ () => alert("SKIP: TO DO") } ></i>
-			</div>
-			</div>
-			<div  className="synth-button">
-			<div className="inner">
-				<i className="fas fa-stop" onClick={ () => {window.speechSynthesis.cancel();document.getElementById("playbtn").className="fas fa-play"} } ></i>
-			</div>
-			</div>
-
-		{/*TEST BUTTON: ALWAYS STARTS READING OUT*/}
-			<div  className="synth-button">
-			<div className="inner">
-				<i className="fas fa-volume-down" onClick={ () => readOutLoud(toRead) } ></i>
-			</div>
+				<i className={ icon } id="playbtn" onClick={ onClick } ></i>
 			</div>
 		</div>
 	);
 }
 
-export default Synth;
+// print to Log
+const print = toPrint => {
+	const log = document.getElementById("log");
+	log.insertAdjacentHTML("beforeend", `<small><b>${toPrint}</b></small>`);
+}
 
+// not used currently
 const playbutton = (synth, toRead) => {
 	console.log(synth);
 	const playbtn = document.getElementById("playbtn");
--- src/helpers/readOutLoud.js
+++ src/helpers/readOutLoud.js
@@ -4,6 +4,16 @@ const readOutLoud = (message) => {
 
 	const synth = window.speechSynthesis;
 	const voices = synth.getVoices();
+	console.log(voices);
+	console.log(window.speechSynthesis.getVoices());
+
+	const log = document.getElementById("log");
+	
+	for (let i=0; i<voices.length; i++) {
+		log.insertAdjacentHTML("beforeend", `<small><b>name:</b> ${voices[i].name}</small>`);
+		log.insertAdjacentHTML("beforeend", `<small>voiceURI: ${voices[i].voiceURI}</small>`);
+		log.insertAdjacentHTML("beforeend", `<small>localService: ${voices[i].localService}</small>`);
+	}
 
 	// Set text and voice attributes
 	speech.text = message;
@@ -13,15 +23,36 @@ const readOutLoud = (message) => {
 	speech.voice = voices[0];
 
 	// Listen for Events
-	speech.onstart = () => console.log("SPEECH START!");
-	speech.onend = () => console.log("SPEECH END!");
-	speech.onpause = () => console.log("SPEECH PAUSE!");
-	speech.onresume = () => console.log("SPEECH RESUME!");
-	speech.onerror = () => console.log("SPEECH ERROR!");
+	speech.onstart = () => {
+		console.log("SPEECH START!");
+		print("SPEECH started.");
+	}
+	speech.onend = () => {
+		console.log("SPEECH END!");
+		print("SPEECH ended.");
+	}
+	speech.onpause = () => {
+		console.log("SPEECH PAUSE!");
+		print("SPEECH paused.");
+	}
+	speech.onresume = () => {
+		console.log("SPEECH RESUME!");
+		print("SPEECH resumed.");
+	}
+	speech.onerror = () => {
+		console.log("SPEECH ERROR!");
+		print("SPEECH ERROR!");
+	}
 	
 	// start playback
 	synth.speak(speech);
 }
 
 export default readOutLoud;
+
+// print to Log
+const print = toPrint => {
+	const log = document.getElementById("log");
+	log.insertAdjacentHTML("beforeend", `<small><b>${toPrint}</b></small>`);
+}
     
