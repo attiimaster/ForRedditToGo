343392f95e64ee2c1b2ebcb9d75167405ebfc7c5
Atti <raphael_mayer@hotmail.de>
2018-09-29 15:55:19 +0200

    fixed ui overflow issue

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -91,7 +91,7 @@ const Sections = () => {
         <h1>Listen to Reddit, while walking, cooking or whatever.</h1>
         <p>Reddit To Go is a web application intended for portable devices that leverages the <b>Text to Speech</b> Feature of modern browsers to read out reddit threads. For now please consider using <b>Google Chrome</b> or <b>Mozilla Firefox</b> to ensure the best possible user experience.</p>
         <p><b>Please note:</b> This is not a substitute for browsing reddit. Although threads with links, images and videos can be accessed, it's main focus are textbased threads.</p>
-        <p>Features like messaging, mod features, etc are not included for now because I do not want to create another reddit client, but a complementary web service one can use if needed.</p>
+        <p>Features like messaging, mod features, etc are not included for now, because this is meant to be a complementary service one can use if needed.</p>
         <p>If you want to suggest or contribute a feature, feel free to contact me on my page or on github.</p> 
       </section>
   
@@ -104,7 +104,7 @@ const Sections = () => {
   
       <section className="frontpage-section">
         <h1>Contact</h1>
-        <p>If you find any bugs or have suggestions please feel free to contact me on my page or on github. I built this project for myself initially, but if people find it useful I would be stoked to improve <b>For Reddit, To Go</b> for others aswell.</p>
+        <p>If you find any bugs or have suggestions please feel free to contact me on my page or on github. I built this project for myself initially, but figured that some people might find it useful I would be stoked to improve <b>For Reddit, To Go</b> for others aswell.</p>
         
         <div className="socialmedia-icons">
           <a href="https://attiimaster.github.io/go" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope"></i></a>
