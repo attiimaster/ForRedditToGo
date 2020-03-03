2ab6d755b9d7d5cacef658a169a1b8bd663c8e79
Raphael Mayer <35350266+raphaelmayer@users.noreply.github.com>
2020-03-03 08:33:47 +0100

    Updates

---
+++ precache-manifest.a78b7d54becb7327d857254f7c077a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8743c356ebe10fc2624a41ee53ac4e",
+    "url": "/ForRedditToGo/index.html"
+  },
+  {
+    "revision": "d6a601e18d2980e65c55",
+    "url": "/ForRedditToGo/static/css/main.ebb39c69.chunk.css"
+  },
+  {
+    "revision": "10fa29a4f1824b95142e",
+    "url": "/ForRedditToGo/static/js/2.06102c6e.chunk.js"
+  },
+  {
+    "revision": "86d636beefada6bc79b09a2076b71fff",
+    "url": "/ForRedditToGo/static/js/2.06102c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a601e18d2980e65c55",
+    "url": "/ForRedditToGo/static/js/main.571bae54.chunk.js"
+  },
+  {
+    "revision": "0a825d16bb4c2c779f20",
+    "url": "/ForRedditToGo/static/js/runtime-main.07bddf95.js"
+  }
+]);
